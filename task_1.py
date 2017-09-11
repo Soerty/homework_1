a89e62f31df827460438a05fe7be48d8d4bfca1f
Светлана Малиновская <soerty97@gmail.com>
2017-09-11 21:17:02 +0300

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,34 @@
+#-*- coding: utf-8 -*-
+
+
+def fibonachi(n):
+    """The calculation of Fibonacci numbers for n > 0.
+
+    If the number is greater than zero, then returns the Fibonacci number.
+    Else return 0.
+
+    >>> fibonachi(-10)
+    0
+    >>> fibonachi(1)
+    1
+    >>> fibonachi(10)
+    55
+    """
+
+    if n < 1:
+        return 0
+
+    before_last = 0
+    last = 1
+    result = 1
+
+    for i in range(2, n+1):
+        result = before_last + last
+        before_last, last = last, result
+
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
